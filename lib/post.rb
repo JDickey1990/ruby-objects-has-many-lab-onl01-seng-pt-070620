cf2911ee317bc2c47c4a2684893912ec18174755
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-08-04 01:02:13 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,12 +1,20 @@
 class Post 
   
-  attr_accessor :title
+  attr_accessor :title, :author
   
   @@all =[]
   
   def initialize(title)
     @title = title 
      save
+  end
+  
+  def save 
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all
   end
  
  
